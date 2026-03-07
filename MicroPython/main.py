949b47c107db6435c3334ae6a1cd2370b866f4ac
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-03-07 00:09:10 +0000

    programmed all RGB LED colors in MicroPython

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,60 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Noah
+Created on: March 2026
+This module is a Micro:bit MicroPython program that lights up the LED in all colors available
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+# setting up
+display.clear()
+display.show(Image.HAPPY)
+
+# shows all colors on A button
+while True:
+    if button_a.is_pressed():
+        display.show(Image.YES)
+    # turns on red pin
+    pin14.write_digital(1)
+    sleep(1000)
+    pin14.write_digital(0)
+    # turns on blue pin
+    pin15.write_digital(1)
+    sleep(1000)
+    pin15.write_digital(0)
+    # turns on green pin
+    pin16.write_digital(1)
+    sleep(1000)
+    pin16.write_digital(0)
+
+    # turns on blue and red pin(magenta)
+    pin14.write_digital(1)
+    pin16.write_digital(1)
+    sleep(1000)
+    pin14.write_digital(0)
+    pin16.write_digital(0)
+
+    # turns on green and red pin(yellow)
+    pin14.write_digital(1)
+    pin15.write_digital(1)
+    sleep(1000)
+    pin14.write_digital(0)
+    pin15.write_digital(0)
+
+    # turns on green and blue pin(cyan)
+    pin15.write_digital(1)
+    pin16.write_digital(1)
+    sleep(1000)
+    pin15.write_digital(0)
+    pin16.write_digital(0)
+
+    # turns on green, red and blue pin(white)
+    pin14.write.digital(1)
+    pin15.write_digital(1)
+    pin16.write_digital(1)
+    sleep(1000)
+
+    # turning off everything(no color)
+    pin14.write.digital(0)
+    pin15.write_digital(0)
+    pin16.write_digital(0)
