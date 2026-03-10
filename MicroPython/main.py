d88b9dcadf449831272345f99998516f75763f28
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-03-10 00:16:08 +0000

    showed words corresponding with color

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -14,47 +14,59 @@ display.show(Image.HAPPY)
 while True:
     if button_a.is_pressed():
         display.show(Image.YES)
+
     # turns on red pin
     pin14.write_digital(1)
-    sleep(1000)
+    display.show("red")
     pin14.write_digital(0)
+    display.clear()
+
     # turns on blue pin
     pin15.write_digital(1)
-    sleep(1000)
+    display.show("green")
     pin15.write_digital(0)
+    display.clear()
+
     # turns on green pin
     pin16.write_digital(1)
-    sleep(1000)
+    display.show("blue")
     pin16.write_digital(0)
+    display.clear()
 
     # turns on blue and red pin(magenta)
     pin14.write_digital(1)
     pin16.write_digital(1)
-    sleep(1000)
+    display.show("magenta")
     pin14.write_digital(0)
     pin16.write_digital(0)
+    display.clear
 
     # turns on green and red pin(yellow)
     pin14.write_digital(1)
     pin15.write_digital(1)
-    sleep(1000)
+    display.show("yellow")
     pin14.write_digital(0)
     pin15.write_digital(0)
+    display.clear()
 
     # turns on green and blue pin(cyan)
     pin15.write_digital(1)
     pin16.write_digital(1)
-    sleep(1000)
+    display.show("cyan")
     pin15.write_digital(0)
     pin16.write_digital(0)
+    display.clear
 
     # turns on green, red and blue pin(white)
     pin14.write.digital(1)
     pin15.write_digital(1)
     pin16.write_digital(1)
-    sleep(1000)
+    display.show("white")
+    display.clear
 
     # turning off everything(no color)
     pin14.write.digital(0)
     pin15.write_digital(0)
     pin16.write_digital(0)
+    display.show("no color")
+    display.clear
